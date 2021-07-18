e6cbc72050c12dbb3a2ca599c2a9580fb6ae80b4
Carlos V <cavarod@gmail.com>
2021-07-18 12:20:20 -0700

    testing heroku deploy

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+  type Book {
+    bookId: ID
+    title: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+    bookCount: Int
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookData: BookInput): User
+    removeBook(bookId: ID!): User
+  }
+  
+  type Auth {
+    token: ID!
+    user: User
+  }
+  input BookInput {
+    bookId: ID
+    title: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+  }
+`;
+
+module.exports = typeDefs;
